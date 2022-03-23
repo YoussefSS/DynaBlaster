429c1ec124f3ea73f2da7e7d4b1247ae8039fe09
YoussefSS <joesamir98@gmail.com>
2022-03-24 00:29:40 +0200

    Added PlayerCharacter that moves with WASD in a top down view

---
+++ Source/DynaBlaster/Private/Characters/PlayerCharacter.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/PlayerCharacter.h"
+
+#include "Components\InputComponent.h"
+#include "GameFramework\CharacterMovementComponent.h"
+#include "GameFramework\PlayerController.h"
+#include "EngineUtils.h"
+
+#include "TopDownCamera.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	UWorld* World = GetWorld();
+	if (PC && World)
+	{
+		for (TActorIterator<ATopDownCamera> CameraItr(World); CameraItr; ++CameraItr)
+		{
+			PC->SetViewTarget(*CameraItr);
+		}
+	}
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+}
+
+void APlayerCharacter::MoveForward(float Value)
+{
+	GetCharacterMovement()->AddInputVector(FVector(Value, 0, 0));
+}
+
+void APlayerCharacter::MoveRight(float Value)
+{
+	GetCharacterMovement()->AddInputVector(FVector(0, Value, 0));
+}
+
