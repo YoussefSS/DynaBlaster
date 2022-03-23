71e19c309b4d20b5141c00c75c1858e4a1107733
YoussefSS <joesamir98@gmail.com>
2022-03-23 17:36:37 +0200

    Initial Commit

---
+++ Source/DynaBlaster/DynaBlasterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DynaBlasterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNABLASTER_API ADynaBlasterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
